cf456a1887c96458a03458ddea6c5a3a0f484823
Pranav Sakthivel <pranavsakthivel@outlook.com>
2019-08-08 14:33:56 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.bdada9248181f169d911f5c3486917b2.js"
 );
 
 self.addEventListener('message', (event) => {
